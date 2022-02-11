0bfa238afb840115fe84e815e1df11b67ea254f7
BYUNGSOO <eedock92@naver.com>
2022-02-11 17:28:11 +0900

    3rd

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,7 +31,8 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     const mealsLisst = DUMMY_MEALS.map(meal => 
-    <MealItem key={meal.id} 
+    <MealItem id={meal.id}
+             key={meal.id} 
              name={meal.name} 
              description={meal.description}
              price={meal.price}/>)
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealItemForm from './MealItemForm'
 import classes from './MealItem.module.css'
 
 const MealItem = props => {
@@ -6,12 +7,14 @@ const price = `$${props.price.toFixed(2)}`
 
  return <li className={classes.meal}>
             <div>
-            <h3>{props.name}</h3>
-            <div className={classes.description}>{props.description}</div>
-            <div className={classes.price}>{price}</div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
             </div>
-            <div></div>
- </li>
+            <div>
+                <MealItemForm id={props.id}/>
+            </div>
+        </li>
 }
 
 export default MealItem
